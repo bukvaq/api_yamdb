44d61bd69f2c7fd8e24cd8ac686b1bf3a179cf0d
EugeneSal <umbra-mortis@mail.ru>
2021-09-06 22:06:09 +0800

    Update models.py
    Моделька пользователя

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,5 +2,33 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class UserRole(models.TextChoices):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+
 class User(AbstractUser):
-    pass
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    username = models.CharField(
+        max_length=50, unique=True, null=True,
+        blank=True, verbose_name='Ник-нейм пользователя')
+    role = models.CharField(
+        max_length=20, choices=UserRole.choices,
+        default=UserRole.USER, verbose_name='Роль'
+    )
+    confirmation_code = models.CharField(
+        max_length=100, editable=False, blank=True,
+        null=True, unique=True, verbose_name='Код подтверждения')
+    email = models.EmailField(blank=False, unique=True,
+                              verbose_name='Электронная почта')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
