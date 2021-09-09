a1dd5a7a205f12e466b0f8cd9d8f96e42bd6d569
EugeneSal <umbra-mortis@mail.ru>
2021-09-10 01:46:16 +0800

    52 из 52 первое ревью

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,20 +14,19 @@ class AdminPermissionOrReadOnly(BasePermission):
     """Пермишн для админа, суперадмина,
      и для авторизованного пользователя для метода гет."""
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS or
-                request.user.is_authenticated and
-                (request.user.role == UserRoles.ADMIN
-                 or request.user.is_superuser)
-                )
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated
+            and (request.user.role == UserRoles.ADMIN
+                 or request.user.is_superuser))
 
 
 class ReviewPermission(BasePermission):
     """Пермишн для Ревью и Комментариев."""
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS or
-                request.user.is_authenticated)
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS or
-                obj.author == request.user or
-                request.user.role in ['admin', 'moderator'])
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.role in ['admin', 'moderator'])
